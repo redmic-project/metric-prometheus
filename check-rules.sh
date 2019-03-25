117887a4ffc213cec3041a73efcc816b7e537132
Pedro Trujillo <pedro@redmic.es>
2019-03-25 09:35:03 +0000

    Revisa servicios y redefine límites
    Actualiza parámetros de límite: renombra el de tiempo a nueva
    nomenclatura y añade el de espacio ocupado.
    
    Mueve definición de servicios a consultar a variable de entorno, en
    lugar de usar el fichero de config. Añade nuevos servicios.
    
    Corrige otros detalles menores.

--- check-rules.sh
+++ check-rules.sh
@@ -5,7 +5,7 @@ do
 	if [ -e "$f" ]
 	then
 		filename=$( basename "$f" )
-		docker run -v $(pwd)/deploy/rules/:/tmp dnanexus/promtool:1.0 \
+		docker run --rm -v $(pwd)/deploy/rules/:/tmp dnanexus/promtool:1.0 \
 			check rules /tmp/${filename}
 	fi
 done
